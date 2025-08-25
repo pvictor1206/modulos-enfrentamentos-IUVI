75c3f4d7e95ca01d563dc7ade3bd2f4c16428b55
Paulo Victor Santos Magalhães <paulomagalhaes1206@gmail.com>
2025-08-25 14:33:52 -0300

    commit

--- src/components/BarProgress/ProgressBar.jsx
+++ src/components/BarProgress/ProgressBar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function ProgressBar({ porcentagem }) {
   return (
     <div className="w-full">
-      <div className="w-full bg-gray-200 rounded-full h-1.5">
+      <div className="w-full bg-gray-200 h-1.5">
         <div
-          className="bg-[#C9BFEF] h-1.5 rounded-full transition-all duration-300"
+          className="bg-[#C9BFEF] h-1.5 transition-all duration-300"
           style={{ width: `${porcentagem}%` }}
         ></div>
       </div>
